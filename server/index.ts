5cc27d838d0e1a7fa6c043563c96ee9c0a42d5ff
Przemek <przemek@rudzki.ca>
2022-07-07 22:07:23 -0400

    update index server file

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import { connect } from "./db/connect";
+import taskRouter from "./routes/task";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.end("ok");
-});
+app.use(express.json());
+
+app.use("/tasks", taskRouter);
+
+connect()
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5500, () => {
   console.log("server started");
